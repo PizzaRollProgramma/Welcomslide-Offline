e5c3a880b2331c99da5dcab300144241e8f9096d
Ty Ternus <tyternus@tys-macbook-pro>
2017-02-23 22:50:10 -0600

    Initial Commit

---
+++ Welcomslide Offline/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Welcomslide Offline
+//
+//  Created by Ty Ternus on 2/23/17.
+//  Copyright © 2017 Ty Ternus. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
